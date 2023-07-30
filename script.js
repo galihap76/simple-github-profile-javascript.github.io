8b1f64de376575c1dc7634c256e8d68433c43559
Galih Anggoro Prasetya <83481679+galihap76@users.noreply.github.com>
2023-07-30 15:43:08 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,22 @@
-let avatar = document.getElementsByClassName('avatar')[0];
-let input_search = document.getElementsByTagName('input')[0];
-let btn = document.getElementsByTagName('button')[0];
-let td = document.querySelectorAll('td');
+// Create variables
+let avatar = document.getElementById("avatar");
+let tableData = document.querySelectorAll("td");
+let searchInput = document.getElementById("find");
+let button = document.getElementsByTagName("button")[0];
 
-btn.addEventListener('click', function(){
-    fetch('https://api.github.com/users/' + input_search.value)
-    .then(res=>res.json())
-    .then(data=>{
-    avatar.innerHTML = `<img src="${data.avatar_url}" style="width:250px;height:250px;">`;
-    for (let i = 0; i < td.length; i++) {
-        td[0].innerHTML = `<td>${data.name}</td>`;
-        td[1].innerHTML = `<td>${data.bio}</td>`;
-        td[2].innerHTML = `<td>${data.location}</td>`;
-        td[3].innerHTML = `<td>${data.followers}</td>`;
-        td[4].innerHTML = `<td>${data.following}</td>`;
+// When the button is clicked
+button.addEventListener("click", function () {
+  // Whatever is typed by me
+  fetch("https://api.github.com/users/" + searchInput.value)
+    // Parsing JSON data
+    .then((res) => res.json())
+    // Then display the data in the HTML document
+    .then((data) => {
+      avatar.innerHTML = `<img src="${data.avatar_url}" style="width:250px;height:250px;">`;
+      for (let i = 0; i < tableData.length; i++) {
+        tableData[0].innerText = `${data.name}`;
+        tableData[1].innerText = `${data.bio}`;
+        tableData[2].innerText = `${data.location}`;
       }
-    })
+    });
 });
